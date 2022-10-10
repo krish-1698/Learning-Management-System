bf44abaa0bc4b74d374791a5a10179a81d1b5abe
SineruPerera <sineru.perera7@gmail.com>
2022-10-10 23:47:40 +0530

    dashboard changes

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Sidebar from '../../lib/Sidebar/Sidebar'
+import AllCoursesComp from '../../components/AllCoursesComp/AllCoursesComp'
+import { enrolledCoursesListItems } from "../../lib/ListItems/ListItems";
 
 function Dashboard() {
   return (
     <div>
-        <Sidebar title={"Dashboard"}/>
+        {/* <Sidebar title={"Dashboard"}/> */}
+        {/* <Sidebar title={"Dashboard"} component={<AllCoursesComp />} /> */}
+        <Sidebar title={"Dashboard"} component={enrolledCoursesListItems} />
     </div>
   )
 }
--- src/pages/Enrollments/Enrollments.jsx
+++ src/pages/Enrollments/Enrollments.jsx
@@ -8,7 +8,7 @@ import StudentsEvoluation from "../../lib/StudentsEvoluation/StudentsEvoluation"
 function Enrollments() {
   // enrollement page number 1 => View all enrollments for both lecturer and student
   // enrollement page number 2 => Lecturer side course anouncements and student evoluation
-  const [enrollPageNumber, setEnrollPageNumber] = useState(3);
+  const [enrollPageNumber, setEnrollPageNumber] = useState(1);
 
   return (
     <Box>
