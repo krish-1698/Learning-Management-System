e83f110e494150e9dcf87dbda04e959170bb18b8
Dilmi <dkaushani98@gmail.com>
2022-10-12 16:29:44 +0530

    Register created

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import {
-  MDBContainer,
-  MDBInput,
-  MDBCheckbox,
-  MDBBtn
-}
-  from 'mdb-react-ui-kit';
-
+import React from "react";
+import { MDBContainer, MDBInput, MDBCheckbox, MDBBtn } from "mdb-react-ui-kit";
 
 function Login() {
   return (
-    <div><MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+    <div>
+      <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+        <MDBInput
+          wrapperClass="mb-4"
+          label="Email address"
+          id="form1"
+          type="email"
+        />
+        <MDBInput
+          wrapperClass="mb-4"
+          label="Password"
+          id="form2"
+          type="password"
+        />
 
-      <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email' />
-      <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password' />
-
-      <div className="d-flex justify-content-between mx-3 mb-4">
-        <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-        <a href="!#">Forgot password?</a>
+        <div className="d-flex justify-content-between mx-3 mb-4">
+          <MDBCheckbox
+            name="flexCheck"
+            value=""
+            id="flexCheckDefault"
+            label="Remember me"
+          />
+          <a href="!#">Forgot password?</a>
+        </div>
+        <MDBBtn className="mb-4">Sign in</MDBBtn>
+      </MDBContainer>
+      <div className="text-center">
+        <p>
+          Not a member?<a href="#!">Register</a>
+        </p>
       </div>
-      <MDBBtn className="mb-4">Sign in</MDBBtn>
-    </MDBContainer><div className="text-center">
-        <p>Not a member?<a href="#!">Register</a></p>
-      </div></div>
-  )
+    </div>
+  );
 }
 
-export default Login
+export default Login;
