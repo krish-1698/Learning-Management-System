bebd0a79a87a67d385a41b2874587b2436e46245
SineruPerera <sineru.perera7@gmail.com>
2022-10-14 19:51:32 +0530

    update dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Sidebar from '../../lib/Sidebar/Sidebar'
-import { allCoursesListItems } from "../../lib/ListItems/ListItems";
-import DashboardCountCard from '../../components/DashboardCountCard/DashboardCountCard';
+import DashboardData from '../../components/DashboardCountCard/DashboardData';
 
 function Dashboard() {
   return (
     <div>
-        <Sidebar title={"Dashboard"} component={allCoursesListItems} />
+        {/* <Sidebar title={"Dashboard"} component={allCoursesListItems} /> */}
         {/* <Sidebar title={"Dashboard"} component={<DashboardCountCard />} /> */}
+        <Sidebar title={"Dashboard"} component={<DashboardData />} />
     </div>
   )
 }
